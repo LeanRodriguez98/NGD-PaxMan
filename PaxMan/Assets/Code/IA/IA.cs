43b7de53554761f9a0417923296c367eeb202703
Leandro Sebastian Rodriguez <lean1998@hotmail.com>
2019-09-26 13:06:20 -0300

    IA now it's a independent class

---
+++ PaxMan/Assets/Code/IA/IA.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class IA
+{
+    public IA(int _states, int _flags)
+    {
+        fsm = new FSM(_states, _flags);
+        pathFinding = new PathFinding();
+        pathStepIndex = 0;
+        currentPath = new List<Vector2>();
+    }
+    public FSM fsm;
+    public PathFinding pathFinding;
+    public int pathStepIndex;
+    public Vector2 currentPosition;
+    public Vector2 destinationPosition;
+    public List<Vector2> currentPath;
+}
--- PaxMan/Assets/Code/Movable/Ghost.cs
+++ PaxMan/Assets/Code/Movable/Ghost.cs
@@ -78,23 +78,6 @@ public class Ghost : MobileEntity
         public Sprite deadSprite;
     }
 
-    [System.Serializable]
-    public class IA
-    {
-        public IA(int _states, int _flags)
-        {
-            fsm = new FSM(_states, _flags);
-            pathFinding = new PathFinding();
-            pathStepIndex = 0;
-            currentPath = new List<Vector2>();
-        }
-        public FSM fsm;
-        public PathFinding pathFinding;
-        public int pathStepIndex;
-        public Vector2 currentPosition;
-        public Vector2 destinationPosition;
-        public List<Vector2> currentPath;
-    }
     protected IA ia;
     [Space(10)]
     [Tooltip("You can see the tiles ID checking the \"Show tiles ID\" toggle on the Map script")]
